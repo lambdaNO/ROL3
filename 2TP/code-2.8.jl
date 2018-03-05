eb0c40965fe6fd3513c864608bcd1b67b3ecda1c
latifmehdi <mehlatif44@gmail.com>
2018-03-05 15:39:36 +0100

    Résolution du 2.8

--- 2TP/code-2.8.jl
+++ 2TP/code-2.8.jl
@@ -10,15 +10,17 @@ function modelImplicite(solverSelected, coef::Dict{Char,Int}, A::Dict{Char,Vecto
     m = Model(solver = solverSelected)
     nbvar = length(coef)
     nbctr = length(A)
-    ## On pose x_i une variable de décision binaire :
-    ### 1 si une usine i est à moins de 25 km de la ville
-    ### 0 sinon
-    ### i \in {A,...,M}
+    #= On pose x_i une variable de décision binaire :
+    x_i = 1 si une usine i est à moins de 25 km de la ville
+          0 sinon
+    i \in {A,...,M}
+     =#
     @variable(m,x[indPosVille],Bin)
-    ## On pose y_i une variable de décision binaire :
-    ### 1 si on implante une usine sur la ville i
-    ### 0 sinon
-    ### i \in {A,...,M}
+    #= On pose y_i une variable de décision binaire :
+    y_i = 1 si on implante une usine sur la ville i
+          0 sinon
+    i \in {A,...,M}
+    =#
     @variable(m,y[indPosVille],Bin)
 
     ## Attention : Il faut utiliser une indexation des coefficients de la fonction objectif par une table associative. Cela permet de se rapprocher au mieux du modele et d'effectuer un parcours sur tout indPosVille
@@ -33,8 +35,8 @@ function imp(m)
     println("> Implantation des usines de Naoned Cola")
     if status == :Optimal
         println("Problème résolu à l'optimalité")
-        println("Nombre optimal de personnes touchées ",getobjectivevalue(m)*1000) # affichage de la valeur optimale
-        println("Liste des villes où l'on doit implanter une usine pour pouvoir conquérir le monde avec notre succulente boisson")
+        println("Nombre optimal de personnes touchées : ",getobjectivevalue(m)*1000) # affichage de la valeur optimale
+        println("Liste des villes où l'on doit implanter une usine pour pouvoir conquérir le monde avec notre succulente boisson : ")
         for i in indPosVille
             if isapprox(getvalue(m[:y][i]),1) print(i," ")
             end
@@ -71,7 +73,7 @@ coef = Dict(
 p = 2
 ## Déclaration de la matrice creuse A - 13 lignes
 indPosVille = ['A':'M'...]
-
+## Déclaration de la matrice creuse
 A = Dict( 'A' => ['A','B','C','D'],
           'B' => ['A','B','C','D','E','F','G'],
           'C' => ['A','B','C','D'],
@@ -86,10 +88,6 @@ A = Dict( 'A' => ['A','B','C','D'],
           'L' => ['H','I','J','K','L','M'],
           'M' => ['H','L','M']
 )
-
-
-
-
 m = modelImplicite(GLPKSolverMIP(),coef,A,indPosVille,p)
 status = solve(m)
 imp(m)
